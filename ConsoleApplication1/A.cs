d1617b9ce0f6a0a79b95ce33f68363f5658728ef
ChuanTang <dungsithanhtroy2191@gmail.com>
2015-06-03 16:57:35 +0700

    The first test

---
+++ ConsoleApplication1/A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    public abstract class A
+    {
+        public abstract void add(A newA);
+
+        public abstract A getIndex(int index);
+
+        public abstract void getInfor();
+       
+    }
+    public class B : A
+    {
+        ArrayList list = new ArrayList();
+        string name;
+        public B(string name)
+        {
+            this.name = name;
+        }
+        public override void add(A newA)
+        {
+            list.Add(newA);
+        }
+        public override A getIndex(int index)
+        {
+            return (A)list[index];
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A _a = new B("hello");
+            _a.add(new B("alo"));
+        }
+    }
+}
